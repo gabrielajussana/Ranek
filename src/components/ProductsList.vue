c651736837303e54d446bbf5b3cb4607b3b2eabd
Gabriela Jussana <gabrielajussana@gmail.com>
2023-03-24 19:16:29 -0300

    feat: create Home and fix components

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+class ProductList extends Vue{
+    
+}
+export default ProductList
+</script>
+<style scoped>
+    
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <section>
+    <h1>Compre ou Venda</h1>
+    <SearchProduct />
+  </section>
 </template>
 
 <script>
-export default {
-  name: "home"
-};
+import { Component, Vue } from 'vue-property-decorator'
+import ProductList from '@/components/ProductsList.vue'
+import SearchProduct from '@/components/SearchProduct.vue'
+
+@Component({components: { SearchProduct, ProductList}})
+class Home extends Vue {
+  
+}
+export default Home
 </script>
+<style scoped>
+  h1 {
+    font-size: 2rem;
+    color: #87F;
+    margin-top: 40px;
+    text-align: center;
+  }
+</style>
